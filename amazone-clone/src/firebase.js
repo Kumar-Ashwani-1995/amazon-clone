2ca6f1c851b2a64fce40c369ddf2a19ddc26476b
Kumar-Ashwani-1995 <ash123.ak70@gmail.com>
2021-09-14 16:52:12 +0530

    navbar

---
+++ amazone-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDS_XdJal3zV3LrNDcABMEu2jfvhTVVLso",
+    authDomain: "challenge-df3e6.firebaseapp.com",
+    projectId: "challenge-df3e6",
+    storageBucket: "challenge-df3e6.appspot.com",
+    messagingSenderId: "759344290991",
+    appId: "1:759344290991:web:10a419ae610094e70d485a",
+    measurementId: "G-VL2QCH6M6M"
+};
